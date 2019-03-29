d2be0decde46c663480cafadcc88ea6daa82ea53
dislersd <dislersd@gmail.com>
2019-03-29 16:39:55 -0700

    installed express starting to build restful api

--- sso-functions/functions/index.js
+++ sso-functions/functions/index.js
@@ -1,48 +1,52 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello Dylan");
-});
 
-exports.getShouts = functions.https.onRequest((req, res) => {
+const express = require("express");
+const app = express();
+
+app.get("/shouts", (req, res) => {
   admin
-  .firestore()
-  .collection('shouts')
-  .get()
-  .then(data => {
-    let shouts = [];
-    data.forEach(doc => {
-      shouts.push(doc.data());
-    });
-    return res.json(shouts);
-  })
-  .catch(err => console.error(err))
+    .firestore()
+    .collection("shouts")
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      let shouts = [];
+      data.forEach(doc => {
+        shouts.push({
+          shoutId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
+      });
+      return res.json(shouts);
+    })
+    .catch(err => console.error(err));
 });
 
-exports.createShout = functions.https.onRequest((req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(400).json({ message: 'error Method not allowed'})
-  }
+app.post('/shout', (req, res) => {
   const newShout = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
   admin
-  .firestore()
-  .collection('shouts')
-  .add(newShout)
-  .then(doc => {
-    res.json({message: `document ${doc.id} created successfully`})
-  })
-  .catch(err => {
-    res.status(500).json({ error: 'error posting' });
-    console.error(err)
-  })
+    .firestore()
+    .collection("shouts")
+    .add(newShout)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "error posting" });
+      console.error(err);
+    });
 });
+
+// https://baseurl.com/api/shouts
+
+exports.api = functions.https.onRequest(app);
