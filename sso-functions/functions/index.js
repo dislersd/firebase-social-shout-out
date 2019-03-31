cf5560f624490917cf99403139e39b4b27d05259
dislersd <dislersd@gmail.com>
2019-03-30 19:06:50 -0700

    abstracted config to own file - importing to index

--- sso-functions/functions/index.js
+++ sso-functions/functions/index.js
@@ -4,14 +4,7 @@ const app = require("express")();
 
 admin.initializeApp();
 
-var config = {
-  apiKey: "AIzaSyD3RCWIpkLGwT8IIWwUluk4WmalJOE_waw",
-  authDomain: "social-shout-out.firebaseapp.com",
-  databaseURL: "https://social-shout-out.firebaseio.com",
-  projectId: "social-shout-out",
-  storageBucket: "social-shout-out.appspot.com",
-  messagingSenderId: "510630727787"
-};
+const config = require('./config.js')
 
 const firebase = require("firebase");
 firebase.initializeApp(config);
