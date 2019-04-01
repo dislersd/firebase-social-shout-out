b59c8c36c35bdf3d8e0b18100574d7b8a6a0da18
dislersd <dislersd@gmail.com>
2019-03-31 20:41:40 -0700

    added validation for empty fields

--- sso-functions/functions/index.js
+++ sso-functions/functions/index.js
@@ -49,9 +49,18 @@ app.post("/shout", async (req, res) => {
   }
 });
 
-const isEmpty = (string) => {
-  string.trim() === '' ? true : false;
-}
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // if(email.match(regEx)) return true;
+  // else return false;
+  email.match(regEx) ? true : false;
+};
+
+const isEmpty = string => {
+  // if (string.trim() === "") return true;
+  // else return false;
+  string.trim() === "" ? true : false;
+};
 
 // sign up route
 app.post("/signup", (req, res) => {
@@ -61,6 +70,21 @@ app.post("/signup", (req, res) => {
     confirmedPassword: req.body.confirmedPassword,
     handle: req.body.handle
   };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty";
+  if (newUser.password !== newUser.confirmedPassword)
+    errors.confirmedPassword = "Passwords must match";
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   //TODO: Validate Data
   let token, userId;
