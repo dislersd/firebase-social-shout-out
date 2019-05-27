76b9a4d907484df949b48a4b5994c7d4c692ef47
dislersd <dislersd@gmail.com>
2019-05-26 23:00:07 -0700

    added login route

--- sso-functions/functions/index.js
+++ sso-functions/functions/index.js
@@ -53,16 +53,16 @@ const isEmail = email => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.match(regEx)) return true;
   else return false;
-  // email.match(regEx) ? true : false; - ternary not working. Why?
+  // email.match(regEx) ? true : false; - ternary not working.
 };
 
 const isEmpty = string => {
   if (string.trim() === "") return true;
   else return false;
-  // string.trim() === "" ? true : false;  - ternary not working. Why?
+  // string.trim() === "" ? true : false;  - ternary not working.
 };
 
-// sign up route
+// SIGN UP
 app.post("/signup", (req, res) => {
   const newUser = {
     email: req.body.email,
@@ -86,7 +86,7 @@ app.post("/signup", (req, res) => {
 
   if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
-  //TODO: Validate Data
+  // VALIDATING DATA
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
@@ -123,6 +123,35 @@ app.post("/signup", (req, res) => {
       } else {
         return res.status(500).json({ error: err.code });
       }
+    });
+});
+
+// LOGIN
+app.post("/login", (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) errors.email = "Must not be empty";
+  if (isEmpty(user.password)) errors.password = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: error.code });
     });
 });
 
