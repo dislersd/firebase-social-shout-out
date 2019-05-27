7df57ae93de4bdc498913974020dedb40a89fad1
dislersd <dislersd@gmail.com>
2019-05-26 23:31:12 -0700

    built authentication middleware

--- sso-functions/functions/index.js
+++ sso-functions/functions/index.js
@@ -32,13 +32,52 @@ app.get("/shouts", async (req, res) => {
   }
 });
 
-app.post("/shout", async (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
+    });
+};
+
+app.post("/shout", FBAuth, async (req, res) => {
   try {
+    if (req.body.body.trim() === "") {
+      return res.status(400).json({ body: "Body must not be empty" });
+    }
+
     const newShout = {
       body: req.body.body,
-      userHandle: req.body.userHandle,
+      userHandle: req.user.handle,
       createdAt: new Date().toISOString()
     };
+
     const doc = await db.collection("shouts").add(newShout);
     res
       .status(201)
